d0cb79498034f5646ed30398312f1e4bacaf491f
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-10-21 16:12:05 +0300

    ex 2.10

--- part 2/the-phonebook/src/App.jsx
+++ part 2/the-phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import Filter from "./components/Filter";
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -8,31 +11,7 @@ const App = () => {
     { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
 
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
   const [searchText, setSearchText] = useState("");
-
-  const addPerson = (event) => {
-    event.preventDefault();
-    setNewName("");
-    setNewNumber("");
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
-      return;
-    }
-
-    const newPerson = { name: newName, number: newNumber };
-
-    setPersons(persons.concat(newPerson));
-  };
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
 
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(searchText.toLowerCase())
@@ -41,34 +20,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with:
-        <input
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-      </div>
-
+      <Filter searchText={searchText} setSearchText={setSearchText} />
       <h2>add new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-      <ul>
-        {filteredPersons.map((person) => (
-          <li key={person.name}>
-            {person.name} {person.number}
-          </li>
-        ))}
-      </ul>
+      <Persons persons={filteredPersons} />
     </div>
   );
 };
--- part 2/the-phonebook/src/components/Total.jsx
+++
@@ -1,6 +0,0 @@
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, p) => s + p.exercises, 0);
-  return <b>total of {total} exercises</b>;
-};
-
-export default Total;
