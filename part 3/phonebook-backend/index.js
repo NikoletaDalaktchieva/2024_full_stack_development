df5276028f8545339752b3bac96077082b84f8df
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-10-28 14:18:14 +0200

    ex 3.22

--- part 3/phonebook-backend/index.js
+++ part 3/phonebook-backend/index.js
@@ -1,120 +1,119 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
-
-morgan.token("body", (req) => {
-  return req.method === "POST" ? JSON.stringify(req.body) : "";
-});
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 const errorHandler = (error, request, response, next) => {
-  console.log("hereee");
-  console.error(error.message);
+  console.log('hereee')
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(morgan("tiny"));
-app.use(express.static("dist"));
-app.use(cors());
-app.use(express.json());
+app.use(morgan('tiny'))
+app.use(express.static('dist'))
+app.use(cors())
+app.use(express.json())
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-const Person = require("./models/person");
-app.get("/api/persons", (request, response) => {
+const Person = require('./models/person')
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
-app.get("/api/info", async (request, response) => {
-  const count = await Person.countDocuments({});
+app.get('/api/info', async (request, response) => {
+  const count = await Person.countDocuments({})
   response.send(`<div>
     <p>Phonebook has info for ${count} people</p>
     <p>${new Date()}</p>
-    </div>`);
-});
+    </div>`)
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
-      if (person) response.json(person);
-      else response.status(404).end();
+      if (person) response.json(person)
+      else response.status(404).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", async (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', async (request, response, next) => {
+  const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "name or number is missing",
-    });
+      error: 'name or number is missing',
+    })
   }
 
-  const existingPerson = await Person.findOne({ name: body.name });
+  const existingPerson = await Person.findOne({ name: body.name })
   if (existingPerson) {
     return response.status(400).json({
-      error: "you're trying to craete user with already existing name",
-    });
+      error: 'you\'re trying to craete user with already existing name',
+    })
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then((savedPerso) => {
-      response.json(savedPerso);
+      response.json(savedPerso)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
   const person = {
     name: name,
     number: number,
-  };
+  }
 
   Person.findByIdAndUpdate(request.params.id, person, {
     new: true,
     runValidators: true,
-    context: "query",
+    context: 'query',
   })
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(unknownEndpoint);
-app.use(errorHandler);
-const PORT = process.env.PORT;
+app.use(unknownEndpoint)
+app.use(errorHandler)
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
